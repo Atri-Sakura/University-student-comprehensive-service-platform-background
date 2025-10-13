35308eb957a600a3c63060c80709959b3669e848
Administrator <1286358763@qq.com>
2025-10-13 09:33:55 +0800

    框架 maven重新加载就可以运行了

---
+++ microservice-models/auth/src/main/java/com/gzu/auth/AuthApplication.java
@@ -0,0 +1,14 @@
+package com.gzu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
