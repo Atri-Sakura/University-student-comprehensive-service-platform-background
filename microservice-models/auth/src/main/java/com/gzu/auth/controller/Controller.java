ceecb1d2604eab28cebc13c1ab4ceabd8cd463ea
Administrator <1286358763@qq.com>
2025-10-13 15:51:22 +0800

    auth端添加jwt配置相关文件

--- microservice-models/auth/src/main/java/com/gzu/auth/controller/Controller.java
+++ microservice-models/auth/src/main/java/com/gzu/auth/controller/Controller.java
@@ -1,14 +1,23 @@
 package com.gzu.auth.controller;
 
+import com.gzu.auth.properties.JwtProperties;
+import com.gzu.common.result.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.gzu.common.result.Result;
 
 @RestController
+@RefreshScope
 @RequestMapping("/auth")
 public class Controller {
+    @Autowired
+    private JwtProperties jwtProperties;
     @RequestMapping("/hello")
     public Result<Integer> hello(){
+        System.out.println(jwtProperties.getAdminTtl());
+        System.out.println(jwtProperties.getAdminSecretKey());
+        System.out.println(jwtProperties.getAdminTokenName());
         return Result.success(1);
     }
 
--- microservice-models/auth/src/main/java/com/gzu/auth/properties/JwtProperties.java
+++ microservice-models/auth/src/main/java/com/gzu/auth/properties/JwtProperties.java
@@ -2,18 +2,22 @@ package com.gzu.auth.properties;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @Data
 public class JwtProperties {
 
     /**
-     * 平台端生成jwt令牌相关配置
+     * 平台端生成jwt令牌相关配置  已经添加nacos云更新配置
      */
-    @Value("")
+    @Value("${JwtProperties.adminSecretKey}")
     private String adminSecretKey;
+    @Value("${JwtProperties.adminTtl}")
     private long adminTtl;
+    @Value("${JwtProperties.adminTokenName}")
     private String adminTokenName;
 
     /**
