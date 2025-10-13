95f75f9183512d33ecb8cfbcf8454d89e999d489
Administrator <1286358763@qq.com>
2025-10-13 10:37:23 +0800

    bootstrap.yml里面添加discovery的配置为dev

--- microservice-models/auth/src/main/java/com/gzu/auth/AuthApplication.java
+++ microservice-models/auth/src/main/java/com/gzu/auth/AuthApplication.java
@@ -2,8 +2,9 @@ package com.gzu.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
 
