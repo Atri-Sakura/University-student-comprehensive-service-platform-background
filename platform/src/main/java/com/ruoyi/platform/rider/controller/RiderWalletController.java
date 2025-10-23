d3398d7457b9cd98fdb4630dd4bfe085da5d54a0
JINXxg <17785872711@163.com>
2025-10-23 09:30:18 +0800

    骑手基本信息controller,骑手钱包controller

---
+++ platform/src/main/java/com/ruoyi/platform/rider/controller/RiderWalletController.java
@@ -0,0 +1,42 @@
+package com.ruoyi.platform.rider.controller;
+
+
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.SecurityUtils;
+import com.ruoyi.platform.domain.RiderWallet;
+import com.ruoyi.platform.domain.vo.RiderWalletBalanceVO;
+import com.ruoyi.platform.service.IRiderWalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/rider/wallet")
+public class RiderWalletController {
+
+    @Autowired
+    private IRiderWalletService riderWalletService;
+
+    /**
+     * 获取当前骑手钱包余额
+     */
+    @GetMapping("/balance")
+    public AjaxResult getRiderWalletBalance(){
+        Long riderId = SecurityUtils.getUserId();
+
+        if (riderId == null) {
+            return AjaxResult.error("未检测到登录用户，请先登录或启用");
+        }
+
+        RiderWallet wallet = riderWalletService.selectRiderWalletByRiderBaseId(riderId);
+
+        if (wallet == null) {
+            return AjaxResult.error("未找到钱包信息");
+        }
+
+        RiderWalletBalanceVO vo = new RiderWalletBalanceVO(wallet.getBalance());
+        return AjaxResult.success("查询成功",vo);
+    }
+
+}
