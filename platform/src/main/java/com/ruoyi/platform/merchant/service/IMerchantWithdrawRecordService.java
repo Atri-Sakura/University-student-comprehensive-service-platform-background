808f76e3105a6cafadf62351bfae6b1a4903de2f
JINXxg <17785872711@163.com>
2025-10-27 09:17:13 +0800

    商家提现记录Service

---
+++ platform/src/main/java/com/ruoyi/platform/merchant/service/IMerchantWithdrawRecordService.java
@@ -0,0 +1,72 @@
+package com.ruoyi.platform.merchant.service;
+
+
+import com.ruoyi.platform.domain.MerchantWithdrawRecord;
+import com.ruoyi.platform.domain.vo.MerchantWithdrawOverviewVO;
+
+import java.util.List;
+
+/**
+ * 商家提现记录 Service 接口
+ *
+ * 定义商家提现记录的业务逻辑操作接口，包括查询、插入、修改与删除等。
+ *
+ * @author Jinx
+ * @date 2025-10-24
+ */
+public interface IMerchantWithdrawRecordService {
+    /**
+     * 获取提现页概览信息
+     *
+     * @param merchantBaseId 商家ID
+     * @return 概览信息
+     */
+    MerchantWithdrawOverviewVO getWithdrawOverview(Long merchantBaseId);
+    /**
+     * 查询商家提现记录
+     *
+     * @param withdrawId 提现记录ID
+     * @return 商家提现记录
+     */
+    MerchantWithdrawRecord selectMerchantWithdrawRecordById(Long withdrawId);
+
+    /**
+     * 查询商家提现记录列表
+     *
+     * @param record 查询条件
+     * @return 商家提现记录集合
+     */
+    List<MerchantWithdrawRecord> selectMerchantWithdrawRecordList(MerchantWithdrawRecord record);
+
+    /**
+     * 新增商家提现记录
+     *
+     * @param record 提现记录对象
+     * @return 结果
+     */
+    int insertMerchantWithdrawRecord(MerchantWithdrawRecord record);
+
+    /**
+     * 修改商家提现记录
+     *
+     * @param record 提现记录对象
+     * @return 结果
+     */
+    int updateMerchantWithdrawRecord(MerchantWithdrawRecord record);
+
+    /**
+     * 批量删除商家提现记录
+     *
+     * @param withdrawIds 要删除的提现记录ID集合
+     * @return 结果
+     */
+    int deleteMerchantWithdrawRecordByIds(Long[] withdrawIds);
+
+    /**
+     * 删除单个商家提现记录
+     *
+     * @param withdrawId 提现记录ID
+     * @return 结果
+     */
+    int deleteMerchantWithdrawRecordById(Long withdrawId);
+}
