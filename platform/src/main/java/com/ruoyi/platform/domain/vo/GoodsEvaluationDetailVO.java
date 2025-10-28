2fcf7df2b7468207b747cd921c95f40cfd0e52df
moli <3530559352@qq.com>
2025-10-28 09:08:12 +0800

    更新评价详情查询及商家拒单

---
+++ platform/src/main/java/com/ruoyi/platform/domain/vo/GoodsEvaluationDetailVO.java
@@ -0,0 +1,32 @@
+package com.ruoyi.platform.domain;
+
+import java.util.List;
+
+/**
+ * 商品评价详情VO：包括评价和图片列表
+ */
+public class GoodsEvaluationDetailVO {
+    private GoodsEvaluation evaluation;
+    private List<GoodsEvaluationImage> images;
+
+    public GoodsEvaluationDetailVO(GoodsEvaluation evaluation, List<GoodsEvaluationImage> images) {
+        this.evaluation = evaluation;
+        this.images = images;
+    }
+
+    public GoodsEvaluation getEvaluation() {
+        return evaluation;
+    }
+
+    public void setEvaluation(GoodsEvaluation evaluation) {
+        this.evaluation = evaluation;
+    }
+
+    public List<GoodsEvaluationImage> getImages() {
+        return images;
+    }
+
+    public void setImages(List<GoodsEvaluationImage> images) {
+        this.images = images;
+    }
+}
