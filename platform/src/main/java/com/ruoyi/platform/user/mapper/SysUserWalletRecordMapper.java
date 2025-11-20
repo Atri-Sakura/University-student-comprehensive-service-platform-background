c6abd2207849dc93830b2c366223285ba4943e33
Administrator <1286358763@qq.com>
2025-11-20 14:27:24 +0800

    用户端用户钱包余额查询

--- platform/src/main/java/com/ruoyi/platform/user/mapper/SysUserWalletRecordMapper.java
+++ platform/src/main/java/com/ruoyi/platform/user/mapper/SysUserWalletRecordMapper.java
@@ -1,5 +1,6 @@
 package com.ruoyi.platform.user.mapper;
 
+import com.ruoyi.platform.domain.UserWallet;
 import com.ruoyi.platform.user.vo.UserWalletRecordVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface SysUserWalletRecordMapper {
     @Select("Select * from user_wallet_record where user_base_id = #{userId} order by trade_time desc")
     List<UserWalletRecordVO> getUserWalletRecordByUserId(Long userId);
+
+    @Select("Select * from user_wallet where user_base_id = #{userId}")
+    UserWallet getUserWalletBalanceByUserId(Long userId);
 }
