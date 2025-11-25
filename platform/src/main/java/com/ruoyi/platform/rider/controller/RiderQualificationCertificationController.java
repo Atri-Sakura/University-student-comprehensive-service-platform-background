cbf9355844fed762fcebd6b72d84745e49f7e3ff
Administrator <1286358763@qq.com>
2025-11-25 11:21:37 +0800

    fix: 修正getUserWalletBalance接口路径格式

---
+++ platform/src/main/java/com/ruoyi/platform/rider/controller/RiderQualificationCertificationController.java
@@ -0,0 +1,10 @@
+package com.ruoyi.platform.rider.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rider/qualificationCertification")
+public class RiderQualificationCertificationController {
+
+}
