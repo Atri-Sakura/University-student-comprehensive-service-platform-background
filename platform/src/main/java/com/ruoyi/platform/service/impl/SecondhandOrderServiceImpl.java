fa430ecd316db5046feced87178c1a52278a4de2
Administrator <1286358763@qq.com>
2025-12-15 22:15:17 +0800

    添加订单商家ID查询和设置功能

--- platform/src/main/java/com/ruoyi/platform/service/impl/SecondhandOrderServiceImpl.java
+++ platform/src/main/java/com/ruoyi/platform/service/impl/SecondhandOrderServiceImpl.java
@@ -214,6 +214,8 @@ public class  SecondhandOrderServiceImpl implements ISecondhandOrderService {
         // 更新商品状态 -> 已售出（status=2）
         int rows2 = secondhandGoodsMapper.updateSecondhandGoodsStatus(goodsId, 2L);
 
+        Long sellerId = secondhandGoodsMapper.selectSellerIdByOrderMainId(order.getOrderMainId());
+        order.setMerchantId(sellerId);
         orderNotifyService.sendOrderFinishNotify(order.getOrderMainId());
 
 
