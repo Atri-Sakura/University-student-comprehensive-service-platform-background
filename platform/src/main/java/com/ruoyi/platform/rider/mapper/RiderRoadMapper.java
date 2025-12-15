34017a54a96f75e867d20e98201fe3532c530dae
Administrator <1286358763@qq.com>
2025-12-15 20:33:48 +0800

    修改骑手配送订单状态查询条件

--- platform/src/main/java/com/ruoyi/platform/rider/mapper/RiderRoadMapper.java
+++ platform/src/main/java/com/ruoyi/platform/rider/mapper/RiderRoadMapper.java
@@ -11,10 +11,10 @@ import java.util.List;
 @Mapper
 public interface RiderRoadMapper {
 
-    @Select("select * from order_delivery od left join order_main om on od.order_main_id = om.order_main_id where rider_id = #{riderBaseId} and delivery_status = 1 and om.order_status in (2,3)")
+    @Select("select * from order_delivery od left join order_main om on od.order_main_id = om.order_main_id where rider_id = #{riderBaseId} and delivery_status = 1 and om.order_status = 3")
     List<OrderDelivery> getgetPickupAddress(Long riderBaseId);
 
-    @Select("select * from order_delivery od left join order_main om on od.order_main_id = om.order_main_id where rider_id = #{riderBaseId} and delivery_status = 2 and om.order_status in (2,3)")
+    @Select("select * from order_delivery od left join order_main om on od.order_main_id = om.order_main_id where rider_id = #{riderBaseId} and delivery_status = 2 and om.order_status = 4")
     List<OrderDelivery> getTargetAddress(Long riderBaseId);
 
     @Select("select user_id from order_main where order_main_id = #{orderMainId}")
